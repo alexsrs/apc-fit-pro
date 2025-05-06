e4b7dfb46961802ef9280299a4dfb068746e47e7
Alex Sandro <alexsrs@gmail.com>
2025-05-06 14:23:58 -0300

    estava funcionando antes

--- apcpro-web/pages/teste.tsx
+++ apcpro-web/pages/teste.tsx
@@ -1,10 +1,5 @@
 const Teste = () => {
-  (
-    <div>
-      <h1>Calma cocada</h1>
-      <div>Esta é uma página de teste!</div>
-    </div>
-  )
+  return <div>Esta é uma página de teste!</div>;
 };
 
 export default Teste;
